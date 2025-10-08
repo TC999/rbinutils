f514c241d2bf80a3570efca65316cd8b9eeb5344
陈生杂物房 <88823709+TC999@users.noreply.github.com>
2025-10-08 18:07:06 +0800

    修复

--- src/tc/strings/src/main.rs
+++ src/tc/strings/src/main.rs
@@ -9,6 +9,12 @@ use scan_plain::print_strings_in_file;
 fn main() {
     let args = Args::parse();
 
+    if let Err(e) = print_strings_in_file(&args.filename, args.min_len) {
+        eprintln!("错误: {}", e);
+        std::process::exit(2);
+    }
+
+
     // 优先尝试对象文件数据段扫描，失败则退回整个文件扫描
     match print_strings_in_object_file(&args.filename, args.min_len) {
         Ok(true) => {}
