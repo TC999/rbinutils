2f8f7fdef4c3cb4a411f50e2f305e792abc20705
陈生杂物房 <88823709+TC999@users.noreply.github.com>
2025-10-08 23:35:25 +0800

    strings：添加帮助

--- src/tc/strings/src/main.rs
+++ src/tc/strings/src/main.rs
@@ -1,16 +1,23 @@
 mod args;
 mod scan_object;
 mod scan_plain;
+mod help;
 
 use args::Args;
 use scan_object::print_strings_in_object_file;
 use scan_plain::print_strings_in_file;
+use help::HELP_TEXT;
 
 fn main() {
+    let raw_args: Vec<String> = std::env::args().collect();
+    if raw_args.iter().any(|arg| arg == "-h" || arg == "--help") {
+        println!("{}", HELP_TEXT);
+        return;
+    }
+
     let args = Args::parse();
 
     if args.scan_object {
-        // 仅当 -d 参数时，使用 object_min_len
         match print_strings_in_object_file(&args.filename, args.object_min_len) {
             Ok(true) => {}
             Ok(false) | Err(_) => {
@@ -21,7 +28,6 @@ fn main() {
             }
         }
     } else {
-        // 普通扫描
         if let Err(e) = print_strings_in_file(&args.filename, args.min_len) {
             eprintln!("错误: {}", e);
             std::process::exit(2);
